b601845b4cf2a8388793d15d60cf92ee869bfc85
Megaruf Bilal K <bk2155453@gmail.com>
2026-09-03 22:30:03 +0530

    Finished Fetching All The Books

--- src/app/view-all-book/view-all-book.ts
+++ src/app/view-all-book/view-all-book.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Book_Details } from '../../Model';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-view-all-book',
@@ -6,4 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './view-all-book.html',
   styleUrl: './view-all-book.css',
 })
-export class ViewAllBook {}
+export class ViewAllBook implements OnInit
+{
+  viewBook = signal<Book_Details[]>([]);
+  
+  ngOnInit()
+  {
+    const getBook = localStorage.getItem("Book_Details");
+    const parsing : Book_Details[] = getBook ? JSON.parse(getBook) : [] ;
+
+    if(parsing.length > 0)
+    {
+      this.viewBook.set(parsing);
+    }
+    else
+    {
+      alert("There Is No Books Available!❌");
+    }
+  }
+}
