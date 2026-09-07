e7ab56304b02f719cd66d0d9b0a25db65051e26b
Megaruf Bilal K <bk2155453@gmail.com>
2026-09-07 23:54:08 +0530

    Added A New Model Called Add_Book_Details Which Is  Used For Adding And Updating Purpose And Book_Details Model Is Used For Fetching Purposes...

--- src/Model.ts
+++ src/Model.ts
@@ -1,9 +1,24 @@
 export interface Book_Details
 {
-  book_id : number,
-  book_name : string,
-  author_name : string,
-  about_book : string,
-  published_date : string,
-  category : string
+  book_Id : number,
+  book_Name : string,
+  author_Name : string,
+  about_Book : string,
+  published_Date : string,
+  category_Id : number,
+  category :
+  {
+    category_Name : string,
+    category_Id : number
+  }
+}
+
+export interface Add_Book_Details
+{
+  book_Id : number,
+  book_Name : string,
+  author_Name : string,
+  about_Book : string,
+  published_Date : string,
+  category_Id : number
 }
